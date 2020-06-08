3dad09389bdab9863bd3c9ac5f2c69271f468a27
Евгений Стефаненко <mixer@clarionlife.net>
2020-06-08 17:11:46 +1000

    4

--- src/Evelogin.php
+++ src/Evelogin.php
@@ -1,14 +1,11 @@
 <?php
 
-namespace KomAuras\Evelo;
-
-use KomAuras\Evelo\Tools;
+namespace KomAuras;
 
 class Evelogin
 {
     public function show(String $Name)
     {
-        $class = new Tools\SuperString();
-        return 'Привет ' . $Name . '<br>Длинна вашего имени (символов): ' . $class->len($Name);
+        return 'test';
     }
 }
